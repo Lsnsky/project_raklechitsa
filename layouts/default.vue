cdfa8876693bb6b9e1840238fd7304d03aefc939
Igor Lisyanskiy <lsnsky@pm.me>
2020-05-06 14:47:02 +0300

    style: подключить шрифты

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,50 +6,22 @@
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Inter';
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
   box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
+
+// /* Основной код для разрешения 1024px */ // @media (min-width : 2560px) { //
+/* стили для больших мониторов с разрешением 4K */ // } // @media (min-width :
+1440px) { // /* стили для больших настольных компьютеров и ноутбуков */ // } //
+@media (max-width : 768px) { // /* стили для планшета */ // } // @media
+(max-width : 425px) { // /* стили для широкого смартфона */ // } // @media
+(max-width : 375px) { // /* стили для среднего смартфона */ // } // @media
+(max-width : 320px) { // /* стили для малого смартфона */ // }
