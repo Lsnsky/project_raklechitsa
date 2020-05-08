60b73bd34f8ce2049ec3ea1cf5ba36285ff1df26
rim1sta <rim1sta@mail.ru>
2020-05-08 16:11:00 +0300

    первая версия блоков cover и cover-about

---
+++ components/Cover.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="cover">
+    <h2 class="cover__title">#РАКЛЕЧИТСЯ</h2>
+    <div class="cover__button">
+      <button-down />
+    </div>
+  </section>
+</template>
+
+<script>
+import Button_down from '@/components/ui/Button_down';
+export default {
+  components: {
+    'button-down': Button_down,
+  },
+};
+</script>
+
+<style scoped>
+.cover {
+  max-width: 1440px;
+  height: 689px;
+  background-color: #613a93;
+  margin: 60px auto;
+  display: flex;
+  flex-direction: column;
+}
+.cover__title {
+  color: #ffffff;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 92px;
+  line-height: 111px;
+  margin: 290px auto 0px;
+}
+.cover__button {
+  width: 36px;
+  height: 12px;
+  margin-top: auto;
+  margin-bottom: 40px;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
---
+++ components/ui/Button_down.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button_down"></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.button_down {
+  display: block;
+  width: 36px;
+  height: 12px;
+  border: none;
+  outline: none;
+  background: center no-repeat
+    url("data:image/svg+xml,%3Csvg width='38' height='15' viewBox='0 0 38 15' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M37 1L19 13L1 1' stroke='white' stroke-width='2'/%3E%3C/svg%3E%0A");
+}
+</style>
