b9ba0f7dd401f2a03b8af5be9858d79fe5530b4d
Igor Lisyanskiy <lsnsky@pm.me>
2020-05-09 16:09:38 +0300

    feat:добавить ProgressBar, переработать блок Statistics

---
+++ components/ui/CardStatContent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="card__content">{{ content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['content'],
+};
+</script>
+
+<style scoped>
+.card__content {
+  /* border: 1px solid red; */
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 38px;
+  line-height: 40px;
+  /* identical to box height, or 105% */
+  text-align: right;
+  margin: 0px 20px 20px;
+}
+</style>
---
+++ components/ui/Diagram.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="diagram">
+    <div class="diagram__empty" :style="styleEmpty"></div>
+    <div class="diagram__fill" :style="styleFill"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['valueEmpty', 'maxValueEmpty', 'valueFill', 'maxValueFill'],
+  computed: {
+    styleEmpty() {
+      return `width: ${(this.valueEmpty / this.maxValueEmpty) * 100}%;`;
+    },
+    styleFill() {
+      return `width: ${(this.valueFill / this.maxValueFill) * 100}%;`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.diagram {
+  /* border: 1px solid red; */
+  /* display: flex;
+  flex-direction: column;
+  justify-content: center; */
+  width: 260px;
+  height: 40px;
+  margin: auto;
+  margin-bottom: 20px;
+}
+.diagram__empty {
+  /* border: 1px solid red; */
+  width: 260px;
+  height: 20px;
+  background: #f4f4f4;
+  margin: auto;
+  margin-left: 0px;
+}
+.diagram__fill {
+  /* border: 1px solid red; */
+  width: 260px;
+  height: 20px;
+  background: #613a93;
+  margin-left: 0px;
+}
+</style>
