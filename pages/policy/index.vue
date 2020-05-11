f0647f9927a0501e31e0e93cc870ab67a430b48e
Dmitrii Belich <dim1999@mail.ru>
2020-05-11 08:53:55 +0300

    feat: Добавить страницу политики конфиденциальности

---
+++ pages/policy/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <policy />
+  </main>
+</template>
+
+<script>
+import Policy from '@/components/Policy';
+export default {
+  components: {
+    policy: Policy,
+  },
+};
+</script>
+
+<style scoped></style>
