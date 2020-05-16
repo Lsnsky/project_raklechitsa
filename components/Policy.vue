57fbbdb8f8766126375a212a8434adca8587562c
Dmitrii Belich <dim1999@mail.ru>
2020-05-16 11:06:57 +0300

    style: Добавить адаптив блоку Политика

--- components/Policy.vue
+++ components/Policy.vue
@@ -34,8 +34,8 @@ export default {
 <style scoped>
 .policy {
   max-width: 780px;
-  margin: 0 auto;
-  padding: 0 330px;
+  margin: 0 auto 230px;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -51,6 +51,7 @@ export default {
   border-top: solid 1px #efefef;
   border-bottom: solid 1px #efefef;
   padding: 30px 0;
+  width: 100%;
 }
 
 .policy__big-list {
@@ -92,5 +93,134 @@ export default {
 .policy__small-list-item {
   margin: 30px 0 0;
   padding: 0 0 0 16px;
+  font-size: 22px;
+  line-height: 30px;
+}
+
+@media screen and (max-width: 1314px) {
+  .policy {
+    max-width: 700px;
+    margin: 0 auto 230px;
+  }
+
+  .policy__main-title {
+    font-size: 34px;
+    line-height: 44px;
+    margin: 88px 0 0;
+  }
+
+  .policy__big-list-item {
+    margin: 88px 0 0;
+    font-size: 20px;
+    line-height: 28px;
+  }
+
+  .policy__title {
+    font-size: 20px;
+    line-height: 28px;
+  }
+
+  .policy__text {
+    font-size: 20px;
+    line-height: 28px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .policy {
+    max-width: 640px;
+    margin: 0 auto 150px;
+  }
+
+  .policy__main-title {
+    font-size: 30px;
+    line-height: 38px;
+    margin: 78px 0 0;
+  }
+
+  .policy__big-list-item {
+    margin: 80px 0 0;
+    font-size: 18px;
+    line-height: 27px;
+  }
+
+  .policy__title {
+    font-size: 18px;
+    line-height: 27px;
+  }
+
+  .policy__text {
+    font-size: 18px;
+    line-height: 27px;
+  }
+
+  .policy__small-list-item {
+    font-size: 18px;
+    line-height: 27px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .policy__main-title {
+    font-size: 30px;
+    line-height: 38px;
+    margin: 78px 0 40px;
+  }
+
+  .policy__big-list-item {
+    margin: 40px 0 0;
+    font-size: 18px;
+    line-height: 27px;
+  }
+}
+
+@media screen and (max-width: 690px) {
+  .policy {
+    max-width: 640px;
+    margin: 0 15px 50px;
+  }
+}
+
+@media screen and (max-width: 450px) {
+  .policy__main-title {
+    padding: 20px 0;
+    font-size: 18px;
+    line-height: 21px;
+    margin: 50px 0 0;
+  }
+
+  .policy__big-list-item {
+    margin: 40px 0 0;
+    font-size: 13px;
+    line-height: 16px;
+  }
+
+  .policy__title {
+    font-size: 13px;
+    line-height: 16px;
+    margin-bottom: 18px;
+  }
+
+  .policy__text {
+    font-size: 13px;
+    line-height: 16px;
+    margin: 0 0 0 -15px;
+  }
+
+  .policy__small-list {
+    font-size: 13px;
+    line-height: 16px;
+  }
+
+  .policy__small-list-item {
+    margin: 20px 0 0;
+    padding: 0 0 0 9px;
+    font-size: 13px;
+    line-height: 16px;
+  }
+
+  .policy__big-list {
+    padding: 0 0 0 15px;
+  }
 }
 </style>
