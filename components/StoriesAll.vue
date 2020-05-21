74f710f33ea2c7cd6a8fbc8bfe3a41878351ab53
Igor Lisyanskiy <lsnsky@pm.me>
2020-05-21 11:43:28 +0300

    style:исправить стили в StoriesAll

--- components/StoriesAll.vue
+++ components/StoriesAll.vue
@@ -64,33 +64,42 @@ export default {
           idx <= this.startIndex + this.itemsPerPage - 1
       );
     },
+    // в разработке
+
+    calcItemPerPage() {
+      if (process.browser) {
+        if (window.innerWidth > 768) {
+          return (this.itemsPerPage = 16);
+        }
+        if (window.innerWidth <= 768) {
+          return (this.itemsPerPage = 12);
+        }
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
 .cards-story {
-  /* border: 1px solid red; */
   display: flex;
   flex-direction: column;
   justify-content: center;
   max-width: 1320px;
   margin: auto;
+  padding: 0 60px;
 }
 .cards-story__container {
-  /* border: 1px solid red; */
   display: grid;
   grid-template-columns: repeat(4, minmax(265px, 300px));
   grid-template-rows: repeat(4, 1fr);
   grid-column-gap: 40px;
-  grid-row-gap: 60px;
+  grid-row-gap: 70px;
   justify-content: center;
-  grid-auto-flow: dense;
   margin: 0 auto;
 }
 
 .cards-story__title {
-  /* border: 1px solid red; */
   font-family: Inter;
   font-style: normal;
   font-weight: 600;
@@ -100,7 +109,6 @@ export default {
   max-width: 413px;
 }
 .cards-story__search {
-  /* border: 1px solid red; */
   display: flex;
   justify-content: space-between;
   align-content: center;
@@ -108,7 +116,6 @@ export default {
   width: 100%;
 }
 .cards-story__search-input {
-  /* border: 1px solid red; */
   /* background: #e5e5e5; */
   width: calc(100% - 228px);
   padding: 0;
@@ -118,8 +125,100 @@ export default {
   overflow: hidden;
 }
 .cards-story__search-button {
-  /* border: 1px solid red; */
   width: 208px;
   height: 46px;
+}
+@media screen and (max-width: 1280px) {
+  .cards-story {
+    padding: 0 50px;
+  }
+  .cards-story__container {
+    display: grid;
+    grid-template-columns: repeat(4, minmax(208px, 265px));
+    grid-template-rows: repeat(4, 1fr);
+    grid-column-gap: 40px;
+    grid-row-gap: 60px;
+    justify-content: center;
+    margin: 0 auto;
+  }
+}
+@media screen and (max-width: 1024px) {
+  .cards-story__container {
+    display: grid;
+    grid-template-columns: repeat(4, minmax(140px, 208px));
+    grid-template-rows: repeat(4, 1fr);
+    grid-column-gap: 30px;
+    grid-row-gap: 46px;
+    justify-content: center;
+    margin: 0 auto;
+  }
+}
+@media screen and (max-width: 768px) {
+  .cards-story {
+    padding: 0 40px;
+  }
+  .cards-story__title {
+    font-size: 24px;
+    line-height: 28px;
+    margin: 80px auto 0;
+    text-align: center;
+    max-width: 380px;
+  }
+  .cards-story__search {
+    margin: 50px auto 60px;
+  }
+  .cards-story__container {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(140px, 216px));
+    grid-template-rows: repeat(4, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 40px;
+    justify-content: center;
+    margin: 0 auto;
+  }
+}
+@media screen and (max-width: 550px) {
+  .cards-story {
+    padding: 0 40px;
+  }
+  .cards-story__search-button {
+    color: transparent;
+    max-width: 46px;
+    background-image: url('/images/Rectangle 1.png');
+  }
+  .cards-story__search-input {
+    width: calc(100% - 52px);
+    margin: 0;
+  }
+
+  .cards-story__container {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(120px, 290px));
+    grid-template-rows: repeat(6, 1fr);
+    grid-row-gap: 30px;
+    justify-content: center;
+    margin: 0 auto;
+  }
+}
+@media screen and (max-width: 320px) {
+  .cards-story {
+    padding: 0 15px;
+  }
+  .cards-story__title {
+    font-size: 18px;
+    line-height: 21px;
+    margin: 50px auto 0;
+  }
+  .cards-story__search {
+    margin: 40px auto 30px;
+  }
+  .cards-story__container {
+    display: grid;
+    grid-template-columns: repeat(1, minmax(140px, 290px));
+    grid-template-rows: repeat(9, 1fr);
+    grid-row-gap: 30px;
+    justify-content: center;
+    margin: 0 auto;
+  }
 }
 </style>
