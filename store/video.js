a7a189a45cd9684ef5e02eb64c1071c2e3b33270
Dmitrii Belich <dim1999@mail.ru>
2020-05-23 16:48:20 +0300

    feat: Вынести действия с блоком Video в store

--- store/video.js
+++ store/video.js
@@ -1,10 +1,31 @@
 export const state = () => ({
+  leftStatus: 'inactive',
+  rightStatus: 'active',
+  id: 0,
   videos: [],
 });
 
 export const mutations = {
   setState(state, { name, value }) {
     return (state[name] = value);
+  },
+  nextId(state) {
+    return (state.id = state.id + 1);
+  },
+  priviousId(state) {
+    return (state.id = state.id - 1);
+  },
+  setLeftInactive(state) {
+    return (state.leftStatus = 'inactive');
+  },
+  setLeftActive(state) {
+    return (state.leftStatus = 'active');
+  },
+  setRightInactive(state) {
+    return (state.rightStatus = 'inactive');
+  },
+  setRightActive(state) {
+    return (state.rightStatus = 'active');
   },
 };
 export const actions = {
@@ -16,10 +37,43 @@ export const actions = {
       });
     });
   },
+  async nextVideo({ commit, getters }) {
+    if (getters.getId < getters.getVideosLength - 1) {
+      await commit('nextId');
+      if (getters.getId === getters.getVideosLength - 1) {
+        await commit('setRightInactive');
+      }
+      await commit('setLeftActive');
+    }
+  },
+  async priviousVideo({ commit, getters }) {
+    if (getters.getId !== 0) {
+      await commit('priviousId');
+      if (getters.getId === 0) {
+        await commit('setLeftInactive');
+      }
+      await commit('setRightActive');
+    }
+  },
 };
 
 export const getters = {
+  getVideosLength(state) {
+    return state.videos.length;
+  },
   getVideos(state) {
     return state.videos;
+  },
+  getId(state) {
+    return state.id;
+  },
+  getCurrentVideo(state) {
+    return state.videos[state.id]['url'];
+  },
+  getLeftStatus(state) {
+    return state.leftStatus;
+  },
+  getRightStatus(state) {
+    return state.rightStatus;
   },
 };
