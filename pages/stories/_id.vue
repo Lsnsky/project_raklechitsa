fec3e142acee7e823a915be35700b024fd779c60
Igor Lisyanskiy <lsnsky@pm.me>
2020-06-06 14:11:58 +0300

    feat:добавить метатеги,сейчас не работает _id

--- pages/stories/_id.vue
+++ pages/stories/_id.vue
@@ -26,10 +26,54 @@ export default {
       return this.$store.getters['storiesData/getCurrentStory'];
     },
   },
-  head() {
+  // head() {
+  //   return {
+  //     title: `${this.story.author} - РАКЛЕЧИТСЯ.РФ`,
+  //   };
+  // },
+  data() {
     return {
-      title: `${this.story.author} - РАКЛЕЧИТСЯ.РФ`,
+      metas: {
+        title: `${this.story.author} - РАКЛЕЧИТСЯ.РФ`,
+        description: `${this.story.author}. РАКЛЕЧИТСЯ.РФ — проект Фонда Хабенского.`,
+        og_image: '?????git',
+        keywords: 'РАКЛЕЧИТСЯ.РФ, раклечится, этонелечится',
+      },
     };
+  },
+  head() {
+    if (this.metas) {
+      return {
+        title: this.metas.title,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: this.metas.description || '',
+          },
+          {
+            hid: 'keywords',
+            name: 'keywords',
+            content: this.metas.keywords || '',
+          },
+          {
+            hid: 'og:title',
+            property: 'og:title',
+            content: this.metas.title || '',
+          },
+          {
+            hid: 'og:description',
+            property: 'og:description',
+            content: this.metas.description || '',
+          },
+          {
+            hid: 'og:image',
+            property: 'og:image',
+            content: this.metas.og_image || '',
+          },
+        ],
+      };
+    }
   },
 };
 </script>
